c9594e44781aac9fb71d746a4e84408a4651eb14
Narasimha Swamy <narasimha81433@gmail.com>
2022-10-19 19:22:54 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,32 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js';
+import userRouter from './routes/user.js';
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRouter);
+
+
+app.get('/', (req,res) => {
+  res.send("App is Running")
+  })
+  
+
+dotenv.config();
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect( process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`did not connect`));
+
+//mongoose.set('useFindAndModify', false);
