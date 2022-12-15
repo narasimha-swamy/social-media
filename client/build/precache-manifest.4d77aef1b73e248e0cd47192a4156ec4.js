31560b36d48092955dd098c18b20a20cb38b53aa
Narasimha Swamy <narasimha81433@gmail.com>
2022-12-16 03:30:08 +0530

    Add files via upload

---
+++ client/build/precache-manifest.4d77aef1b73e248e0cd47192a4156ec4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad201afaf71fbd3ff70b8366f89fc9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cc77fd992d732afa0b",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87cc77fd992d732afa0b",
+    "url": "/static/js/main.5ddef902.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
