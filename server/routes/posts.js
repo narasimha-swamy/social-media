a8f0dbc61031b48d4f0ca809fbd2597d949eb767
Narasimha Swamy <narasimha81433@gmail.com>
2022-12-16 01:46:09 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts, getPostsBySearch, getPost, createPost, updatePost, like, deletePost } from '../controllers/posts.js';
+import { getPosts, getPostsBySearch, getPostsByCreator, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
+router.get('/creator', getPostsByCreator);
 router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.get('/:id', getPost);
@@ -12,6 +13,7 @@ router.get('/:id', getPost);
 router.post('/', auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
-router.patch('/:id/likePost', auth, like);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', commentPost);
 
 export default router;
